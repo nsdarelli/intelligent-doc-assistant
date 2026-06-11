a4fa55ced68536695b9d3ef7d736ee3df28fd736
Naresh Darelli <nareshdarelli3@gmail.com>
2026-06-11 19:39:29 +0530

    Added Embedding logic

---
+++ app/services/vector_service.py
@@ -0,0 +1,16 @@
+import chromadb
+
+class VectorService:
+
+    def __init__(self):
+        self.client = chromadb.PersistentClient(path="data/chroma")
+        self.collection = self.client.get_or_create_collection(name="documents") # Create or get the collection named "documents"
+    
+    # Add documents to the collection with their corresponding embeddings
+    def add_documents(self, ids, documents, embeddings):
+        self.collection.add(ids=ids, documents=documents, embeddings=embeddings.tolist())
+
+    def search(self, query_embedding, top_k=3):
+        return self.collection.query(query_embeddings=[query_embedding.tolist()], n_results=top_k)
+
+    
